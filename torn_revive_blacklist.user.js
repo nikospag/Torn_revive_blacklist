ea682d7bd8c3731a89f6322118c22f0b3918ff5e
nikospag <nikospag2@gmail.com>
2021-11-11 10:07:22 +0200

    Create torn_revive_blacklist.user.js

---
+++ torn_revive_blacklist.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @run-at       document-end
+// @name         Torn revive blacklist
+// @namespace    torn
+// @version      1.0
+// @description  Adds a red background color at the revive icons (at hospital list)
+// @include      https://www.torn.com/*
+// @author       nikospag
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @icon         https://www.torn.com/favicon.ico
+// ==/UserScript==
+
+/* globals jQuery, $, waitForKeyElements */
+
+// Add or remove user IDs between the brackets separated by commas.
+// examlpe : [111111, 222222, 333333]. Here:
+const userBlst = [111111, 222222];
+//------------------------------
+
+for (let i in userBlst) {
+    waitForKeyElements('[href="revive.php?action=revive&ID='+userBlst[i]+'&text_response=1"]', function(jnd){
+        jnd.css("background-color", "red");
+    });
+}
